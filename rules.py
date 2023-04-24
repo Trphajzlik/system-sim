a0ebee33f6cbadbf3e90f94b4a05eda3fb19145a
matej.zacek <matej.zacek@kiwi.com>
2023-04-24 17:06:18 +0200

    Separate interesting constants into separate file

--- rules.py
+++ rules.py
@@ -1,4 +1,5 @@
 from math import floor
+from constants import CAP50, CAP70, CAP90, CAP100, CAP101, RUMOR_IMPACT, AD_IMPACT, AD_DROPOFF, BUS_EFFICIENCY, TICKET_COST
 
 # TODO: Review function `incl`, it is the most important
 # part of our model, so it has to do what we want it
@@ -11,28 +12,29 @@ def cap_opinion(sum_used, max_cap):
     #  - o_2 influences ppl who didn't,
     #    meaning "common" rumors
     ratio = sum_used / max_cap
+    cap = 0
     if ratio < 0.5:
-        return (2, 1.5)
+        cap = CAP50
     elif ratio < 0.7:
-        return (1.5, 1.25)
+        cap = CAP70
     elif ratio < 0.9:
-        return (1, 1)
+        cap = CAP90
     elif ratio < 1.0:
-        return (0.7, 0.85)
+        cap = CAP100
     else: # Some didn't even get into bus
-        return (0.4, 0.7)
+        cap = CAP101
+    return (1+cap, 1+cap*RUMOR_IMPACT)
 
 def ad_opinion(ads):
     # Spending 500.000 will increase mult opinion by 5%
     # Spending 1.000.000 will increase mult opinion by 7.5%
     # Spending 1.500.000 will increase mult opinion by 8.75%
-    # ...> 
     # a_0 = 0
     # a_n = a_{n-1} + 5 / 2^{n-1}
     # Wolfram says that it is equal to
     # 10 - 5/2^n
     # Maybe play with the constant `2` -> 1.8?
-    return 1 + 0.01 * (10 - 5 / (2**floor(0.00001 * ads)))
+    return 1 + 0.01 * ((2 * AD_IMPACT) - AD_IMPACT / (AD_DROPOFF**floor(0.00001 * ads)))
 
 def clamp(i):
     if i < 0:
@@ -60,10 +62,10 @@ def incl(nat_incl, pop_c, used_c, sum_used, max_cap, ads):
 
 
 def ticket_sales(sum_used):
-    return sum_used * 2000
+    return sum_used * TICKET_COST
 
 def expenses(max_cap):
-    return max_cap * 1000
+    return max_cap * TICKET_COST * BUS_EFFICIENCY
 
 # TODO: Think about more spendinding practices!
 # Model can be adjusted that it takes the whole history,
